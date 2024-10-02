81825d79d85017a8c2c5de2b88a1870807fde160
bwbc <bbc5008@gmail.com>
2024-10-02 02:59:45 +0000

    created sprite groups

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     if not pygame.get_init():
         pygame.init()
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player_avatar = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -18,11 +22,15 @@ def main():
                 return
 
         #update stuff
-        player_avatar.update(dt)    
+        #player_avatar.update(dt)
+        for object in updateable:
+            object.update(dt)    
 
         #draw stuff
         screen.fill('black')
-        player_avatar.draw(screen)
+        #player_avatar.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
